b446def632679cbd5ec2ba8d568b7424b8fe6b26
costoxff <900131shawn@gmail.com>
2023-12-03 12:46:57 +0800

    Add shcolor.h
    1. Use bash shell color code

---
+++ shcolor.h
@@ -0,0 +1,26 @@
+// text pattern
+#define RESET       "\033[00m"
+#define NORMAL      "\033[00;97m"
+#define BOLD        "\033[01;97m"
+#define FAINT       "\033[02;97m"
+#define ITALICS     "\033[03;97m"
+#define UNDERLINED  "\033[04;97m"
+
+// color
+#define BLACK       "\033[30m"
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define BLUE        "\033[34m"
+#define MAGENTA     "\033[35m"
+#define CYAN        "\033[36m"
+#define LGRAY       "\033[37m"
+
+#define GRAY        "\033[90m"
+#define LRED        "\033[91m"
+#define LGREEN      "\033[92m"
+#define LYELLOW     "\033[93m"
+#define LBLUE       "\033[94m"
+#define LMAGENTA    "\033[95m"
+#define LCYAN       "\033[96m"
+#define WHITE       "\033[97m"
