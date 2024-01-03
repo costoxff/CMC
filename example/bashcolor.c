a4d44a45463d3bf14c1e71efc3f64e70e8004041
costoxff <900131shawn@gmail.com>
2024-01-03 15:17:19 +0800

    Rewrite bashcolor.h
    1. The reasin is that I think define macro as a const string is non-scalable for code, so I rewire it using another way.

--- example/bashcolor.c
+++ example/bashcolor.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include "bashcolor.h"
 
-#define put_color_str(str, fg, bg) \
-    printf("%s%s%s%s\n", fg, bg, str, RESET)
-
 #define put_fmt_str(str, fmt) \
     printf("%s%s%s\n", fmt, str, RESET)
 
@@ -12,53 +9,49 @@
 
 int main()
 {
-    char *str = "Hellow World";
-
-    put_fmt_str(str, NORMAL);
-    put_fmt_str(str, BOLD);
-    put_fmt_str(str, FAINT);
-    put_fmt_str(str, ITALICS);
-    put_fmt_str(str, UNDERLINED);
+    put_fmt_str("Hello World!", txtfmt(NORMAL,     FG_DEFAULT, BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(BOLD,       FG_DEFAULT, BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(FAINT,      FG_DEFAULT, BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(ITALICS,    FG_DEFAULT, BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(UNDERLINED, FG_DEFAULT, BG_DEFAULT));
 
     next_line;
 
-    put_color_str(str, BLACK, BG_DEFAULT);
-    put_color_str(str, RED, BG_DEFAULT);
-    put_color_str(str, GREEN, BG_DEFAULT);
-    put_color_str(str, YELLOW, BG_DEFAULT);
-    put_color_str(str, BLUE, BG_DEFAULT);
-    put_color_str(str, MAGENTA, BG_DEFAULT);
-    put_color_str(str, CYAN, BG_DEFAULT);
-    put_color_str(str, LGRAY, BG_DEFAULT);
-
-    put_color_str(str, GRAY, BG_DEFAULT);
-    put_color_str(str, LRED, BG_DEFAULT);
-    put_color_str(str, LGREEN, BG_DEFAULT);
-    put_color_str(str, LYELLOW, BG_DEFAULT);
-    put_color_str(str, LBLUE, BG_DEFAULT);
-    put_color_str(str, LMAGENTA, BG_DEFAULT);
-    put_color_str(str, LCYAN, BG_DEFAULT);
-    put_color_str(str, WHITE, BG_DEFAULT);
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_BLACK,    BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_RED,      BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_GREEN,    BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_YELLOW,   BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_BLUE,     BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_MAGENTA,  BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_CYAN,     BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_LGRAY,    BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_GRAY,     BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_LRED,     BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_LGREEN,   BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_LYELLOW,  BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_LBLUE,    BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_LMAGENTA, BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_LCYAN,    BG_DEFAULT));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, FG_WHITE,    BG_DEFAULT));
 
     next_line;
 
-    put_color_str(str, WHITE, BG_BLACK);
-    put_color_str(str, WHITE, BG_RED);
-    put_color_str(str, WHITE, BG_GREEN);
-    put_color_str(str, WHITE, BG_YELLOW);
-    put_color_str(str, WHITE, BG_BLUE);
-    put_color_str(str, WHITE, BG_MAGENTA);
-    put_color_str(str, WHITE, BG_CYAN);
-    put_color_str(str, BLACK, BG_LGRAY);
-
-    put_color_str(str, WHITE, BG_GRAY);
-    put_color_str(str, BLACK, BG_LRED);
-    put_color_str(str, BLACK, BG_LGREEN);
-    put_color_str(str, BLACK, BG_LYELLOW);
-    put_color_str(str, BLACK, BG_LBLUE);
-    put_color_str(str, BLACK, BG_LMAGENTA);
-    put_color_str(str, BLACK, BG_LCYAN);
-    put_color_str(str, BLACK, BG_WHITE);
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_BLACK));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_RED));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_GREEN));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_YELLOW));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_BLUE));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_MAGENTA));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_CYAN));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_LGRAY));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_GRAY));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_LRED));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_LGREEN));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_LYELLOW));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_LBLUE));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_LMAGENTA));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_LCYAN));
+    put_fmt_str("Hello World!", txtfmt(NORMAL, BG_DEFAULT ,BG_WHITE));
 
     return 0;
 }
--- src/bashcolor.h
+++ src/bashcolor.h
@@ -1,47 +1,81 @@
-// text format
-#define RESET           "\033[00m"
-#define NORMAL          "\033[00m"
-#define BOLD            "\033[01m"
-#define FAINT           "\033[02m"
-#define ITALICS         "\033[03m"
-#define UNDERLINED      "\033[04m"
+#define _STRFCT(x) #x
+#define XSTR(x) _STRFCT(x)
+
+#define _txtfmt(attr, fg, bg) \
+    attr;fg;bg
+
+#define txtfmt(attr, fg, bg) \
+    XSTR(\e[_txtfmt(attr, fg, bg)m)
+
+// text attribute
+#define RESET           XSTR(\e[00m)
+#define NORMAL          00
+#define BOLD            01
+#define FAINT           02
+#define ITALICS         03
+#define UNDERLINED      04
 
 // foreground color
-#define DEFAULT         "\033[39m"
-#define BLACK           "\033[30m"
-#define RED             "\033[31m"
-#define GREEN           "\033[32m"
-#define YELLOW          "\033[33m"
-#define BLUE            "\033[34m"
-#define MAGENTA         "\033[35m"
-#define CYAN            "\033[36m"
-#define LGRAY           "\033[37m"
+#define FG_DEFAULT      39
+#define FG_BLACK        30
+#define FG_RED          31
+#define FG_GREEN        32
+#define FG_YELLOW       33
+#define FG_BLUE         34
+#define FG_MAGENTA      35
+#define FG_CYAN         36
+#define FG_LGRAY        37
 
-#define GRAY            "\033[90m"
-#define LRED            "\033[91m"
-#define LGREEN          "\033[92m"
-#define LYELLOW         "\033[93m"
-#define LBLUE           "\033[94m"
-#define LMAGENTA        "\033[95m"
-#define LCYAN           "\033[96m"
-#define WHITE           "\033[97m"
+#define FG_GRAY         90
+#define FG_LRED         91
+#define FG_LGREEN       92
+#define FG_LYELLOW      93
+#define FG_LBLUE        94
+#define FG_LMAGENTA     95
+#define FG_LCYAN        96
+#define FG_WHITE        97
 
 // background color
-#define BG_DEFAULT      "\033[49m"
-#define BG_BLACK        "\033[40m"
-#define BG_RED          "\033[41m"
-#define BG_GREEN        "\033[42m"
-#define BG_YELLOW       "\033[43m"
-#define BG_BLUE         "\033[44m"
-#define BG_MAGENTA      "\033[45m"
-#define BG_CYAN         "\033[46m"
-#define BG_LGRAY        "\033[47m"
+#define BG_DEFAULT      49
+#define BG_BLACK        40
+#define BG_RED          41
+#define BG_GREEN        42
+#define BG_YELLOW       43
+#define BG_BLUE         44
+#define BG_MAGENTA      45
+#define BG_CYAN         46
+#define BG_LGRAY        47
 
-#define BG_GRAY         "\033[100m"
-#define BG_LRED         "\033[101m"
-#define BG_LGREEN       "\033[102m"
-#define BG_LYELLOW      "\033[103m"
-#define BG_LBLUE        "\033[104m"
-#define BG_LMAGENTA     "\033[105m"
-#define BG_LCYAN        "\033[106m"
-#define BG_WHITE        "\033[107m"
+#define BG_GRAY         100
+#define BG_LRED         101
+#define BG_LGREEN       102
+#define BG_LYELLOW      103
+#define BG_LBLUE        104
+#define BG_LMAGENTA     105
+#define BG_LCYAN        106
+#define BG_WHITE        107
+
+enum text_color {
+    DEFAULT = 39,
+    BLACK = 30,
+    RED,
+    GREEN,
+    YELLOW, 
+    BLUE,
+    MAGENTA,
+    CYAB,
+    LGRAY,
+    GRAY = 90,
+    LRED,
+    LGREEN,
+    LYELLOW,
+    LBLUE,
+    LMAGENTA,
+    LCYAN,
+    WHITE,
+};
+
+#define fg_color(color) color;
+#define bg_color(color) color + 10;
+
+#define itoa(strbuf, nm) sprintf(strbuf, "%d", nm);
