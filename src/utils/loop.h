e9063e8751d7634475acb5c1fe606a38808c19ee
costoxff <900131shawn@gmail.com>
2024-06-24 23:44:21 +0800

    Fix macro expansion issue
    insert parenthesis to all arguments of macro in "loop.h"

--- src/utils/loop.h
+++ src/utils/loop.h
@@ -6,13 +6,13 @@
 	for ((i) = (begin); (i) != (end); (i) += (step))
 
 #define for_each(i, size) for_args(i, 0, size, 1)
-#define for_each_rev(i, size) for_args(i, size - 1, -1, -1) // reverse
+#define for_each_rev(i, size) for_args(i, (size)-1, -1, -1) // reverse
 
 #define _ARRAY_SIZE(arr) (sizeof(arr)/sizeof((arr)[0])) // no type checking
 #define for_each_arr(ptr, arr) \
-	for (ptr = arr; ptr != &arr[_ARRAY_SIZE(arr)]; ptr++)
+	for ((ptr) = (arr); (ptr) != &(arr)[_ARRAY_SIZE(arr)]; (ptr)++)
 
 #define for_each_arr_rev(ptr, arr) \
-	for (ptr = &arr[_ARRAY_SIZE(arr)-1]; ptr != arr - 1; ptr--)
+	for ((ptr) = &(arr)[_ARRAY_SIZE(arr)-1]; (ptr) != (arr) - 1; (ptr)--)
 
 #endif
